6a5be66a696caf466037041ac6e99b496a29e19f
CorbinJenkins <71853790+CorbinJenkins@users.noreply.github.com>
2020-11-09 16:44:00 -0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(body1,point2){
+        var options={
+            bodyA: body1,
+            pointB: point2,
+            length: 1,
+            stiffness: 0.04
+        }
+        this.pointB = point2;
+        this.sling = Constraint.create(options);
+        World.add(world,this.sling);
+    }
+
+    display(){
+        if (this.sling.bodyA){
+            var ptA = this.sling.bodyA.position;
+            var ptB = this.pointB;
+        
+            strokeWeight(4);
+            line(ptA.x,ptA.y,ptB.x,ptB.y);
+        }
+        
+    }
+    
+    fly(){
+        this.sling.bodyA=null;
+    }
+}
